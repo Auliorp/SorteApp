bbaa591bc6875dffe9d649eed4729a2600ea4b5a
Aulio Rovero <aulioroveroparedes@gmail.com>
2025-01-28 13:09:19 -0300

    validations performed on inputs and refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, AbstractControl } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
@@ -17,7 +17,7 @@ import confetti from 'canvas-confetti';
     MatProgressSpinnerModule,
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'sorteApp';
@@ -25,9 +25,9 @@ export class AppComponent {
   results: number[] = [];
   isLoading: boolean = false;
   minValue: number | null = null;
-  maxLimit: number | null = null;
   
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder) {
     this.createForm();
     this.setupFormListeners();
   };
@@ -40,7 +40,8 @@ export class AppComponent {
       min: ['', 
         [
           Validators.required, 
-          Validators.pattern(/^\d+$/)
+          Validators.pattern(/^\d+$/),
+          Validators.min(0)
         ]
       ],
       max: [
@@ -55,26 +56,29 @@ export class AppComponent {
       ],
       winners: [
         { 
-          value: '', 
+          value: null, 
           disabled: true 
         }, 
         [
           Validators.required, 
-          Validators.pattern(/^\d+$/), 
+          Validators.pattern(/^\d+$/),
+          Validators.max(30),
+          Validators.min(1),
         ]
       ],
     });
   };
 
+  
+
   private setupFormListeners(): void {
-    this.form.get('min')?.valueChanges.subscribe((min) => this.handleMinChange(min));
-    this.form.get('max')?.valueChanges.subscribe((max) => this.handleMaxChange(max));
+    this.controls.min?.valueChanges.subscribe((value) => this.handleMinChange(value));
+    this.controls.max?.valueChanges.subscribe((value) => this.handleMaxChange(value));
+    this.controls.winners?.valueChanges.subscribe((value) => this.handleWinnersValueChange(value));
   };
 
   private handleMinChange(min: string): void {
-    const minControl = this.form.get('min');
-    const maxControl = this.form.get('max');
-    const winnersControl = this.form.get('winners');
+    const { min: minControl, max: maxControl, winners: winnersControl } = this.controls;
   
     if (minControl?.valid) {
       maxControl?.enable();
@@ -85,18 +89,26 @@ export class AppComponent {
   };
   
   private handleMaxChange(max: string): void {
-    console.log(max ? +max - 1 : 0)
-    const minControl = this.form.get('min');
-    const maxControl = this.form.get('max')
-    const winnersControl = this.form.get('winners');
+    const { min: minControl, max: maxControl, winners: winnersControl } = this.controls;
     const min =  minControl?.value;
 
-    this.maxLimit = max ? +max - 1 : 0;
     this.minValue = min + 2;
 
     minControl?.valid && maxControl?.valid 
     ? winnersControl?.enable() 
     : winnersControl?.disable();
+  }
+
+  private handleWinnersValueChange(value: number): void {
+    const { max, winners } = this.controls;
+    const maxValue = max?.value;
+    
+    if(value === maxValue || value > maxValue && maxValue <= 30 ){
+      winners?.setValue(maxValue - 1);
+    }
+    if (maxValue > 30 && (value >= maxValue || value > 30)) {
+      winners?.setValue(30);
+    }
   }
 
   onSubmit() {
@@ -125,18 +137,19 @@ export class AppComponent {
     return Array.from(generatedNumbers);
   }
 
+
+  private launchConfetti(): void {
+    confetti({
+      particleCount: 200,
+      spread: 70,
+      origin: { y: 0.6 },
+      colors: ['#ff0000', '#00ff00', '#0000ff'],
+    });
+  }
+
   private simulateProcessing(numbers: number[]): void {
-    const launchConfetti = () => {
-      confetti({
-        particleCount: 200,
-        spread: 70,
-        origin: { y: 0.6 },
-        colors: ['#ff0000', '#00ff00', '#0000ff'],
-      });
-    };
-  
     setTimeout(() => {
-      launchConfetti();
+      this.launchConfetti();
       this.results = numbers;
       this.isLoading = false;
     }, 2000);
@@ -148,67 +161,26 @@ export class AppComponent {
       event.preventDefault();
     }
   }
+
   onReset() {
     this.form.reset({
       min: '',
       max: '',
-      winners: ''
+      winners: null,
     });
-    
-    this.results = [];
-    this.minValue = null;
-    this.maxLimit = null;
-  }
-
-  get maxWinnersValue(): number {
-    const max = this.form.get('max')?.value || 0;
-    return max > 1 ? max - 1 : 0;
   }
 
   get disableButtonClean(): boolean {
-    const minValue = this.form.get('min')?.value;
-    const maxValue = this.form.get('max')?.value;
-    const winnerValue =this.form.get('winner')?.value
-  
-    return !minValue && !maxValue && !winnerValue
+    const { min, max, winners } = this.controls;
+
+    return !min?.value && !max?.value && !winners?.value;
   }
 
-  get exceedsNumberWinners(): boolean {
-    const winnersValue = this.form.get('winners')?.value || 0;
-    const maxValue = this.form.get('max')?.value || 0;
-    return winnersValue > 30 && maxValue > 30;
+  get controls() {
+    return {
+      min: this.form.get('min'),
+      max: this.form.get('max'),
+      winners: this.form.get('winners'),
+    };
   }
-  
-  get equalAmounts(): boolean {
-    const equalValue = this.form.get('max')?.value === this.form.get('winners')?.value;
-    return equalValue;
-  }
-
-  get equalMaxAndWinnerValue(): boolean {
-    const maxValue = this.form.get('max')?.value;
-    const winnersValue = this.form.get('winners')?.value;
-    return maxValue === 30 && winnersValue === 30;
-  }
-  
-  get exceedsMaxLimit(): boolean {
-    const winnersValue = this.form.get('winners')?.value || 0;
-    const maxValue = this.maxLimit || 0;
-    if(winnersValue > maxValue){
-      return true
-    }
-    return false
-  }
-
-  get exceedsWinnerLimit(): boolean {
-    const winnersValue = this.form.get('winners')?.value
-    if (winnersValue > 30){
-     return true
-    }
-    return false
-  }
-
-  get returnThirty(): number{
-    return 30
-  }
-
 }
